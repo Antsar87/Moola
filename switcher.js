b312465a16b7362f55d2dca587899995dfd2864c
antsar <jomay1129@gmail.com>
2022-03-08 16:51:53 -0600

    css fixes

--- switcher.js
+++ switcher.js
@@ -1,18 +1,33 @@
 const switcher = document.querySelector('.theme-switcher');
 const body = document.querySelector('body');
 
+const sun = () => {
+  switcher.innerHTML = "<img src='img/sun-solid.svg' />";
+};
+
+const moon = () => {
+  switcher.innerHTML = "<img src='img/moon-solid.svg' />";
+};
+
 if (localStorage.getItem('theme') === 'light') {
   body.classList.add('light-theme');
+  moon();
+}
+
+if (localStorage.getItem('theme') === 'dark') {
+  sun();
 }
 
 switcher.addEventListener('click', () => {
   body.classList.toggle('light-theme');
 
   if (localStorage.getItem('theme') === 'light') {
-    body.classList.remove('light-theme');
     localStorage.setItem('theme', 'dark');
+    sun();
+
     return;
   }
 
   localStorage.setItem('theme', 'light');
+  moon();
 });
