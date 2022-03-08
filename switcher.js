510e80bc44ed5a9514dd3e3ff0cbb548872bc1bd
antsar <jomay1129@gmail.com>
2022-03-08 13:56:46 -0600

    Inflation and Fund pages white added

--- switcher.js
+++ switcher.js
@@ -1,6 +1,18 @@
 const switcher = document.querySelector('.theme-switcher');
-const body = document.querySelector("body")
+const body = document.querySelector('body');
 
-switcher.addEventListener("click", () => {
-    body.classList.toggle("light-theme")
-})
+if (localStorage.getItem('theme') === 'light') {
+  body.classList.add('light-theme');
+}
+
+switcher.addEventListener('click', () => {
+  body.classList.toggle('light-theme');
+
+  if (localStorage.getItem('theme') === 'light') {
+    body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark');
+    return;
+  }
+
+  localStorage.setItem('theme', 'light');
+});
